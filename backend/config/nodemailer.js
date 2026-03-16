4f5ed151ffca67f47adca27fdeba7e70e5a9c4f4
Deep Prajapati <dvprajapati7212@gmail.com>
2026-03-16 12:39:10 +0530

    fix: change smtp port brevo

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -4,6 +4,7 @@ import logger from '../utils/logger.js';
 const transporter = nodemailer.createTransport({
     host: process.env.BREVO_SMTP_HOST,
     port: process.env.BREVO_SMTP_PORT,
+    secure:true,
     auth: {
         user: process.env.BREVO_SMTP_USER,
         pass: process.env.BREVO_SMTP_PASS
