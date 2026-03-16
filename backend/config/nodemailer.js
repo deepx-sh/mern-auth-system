7a483e6f018b92822805c9749d10b24af29caf4f
Deep Prajapati <dvprajapati7212@gmail.com>
2026-03-16 11:14:12 +0530

    fix: update email sending implementation to use Brevo SMTP and remove SendGrid integration

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,35 +1,30 @@
 import nodemailer from 'nodemailer';
-import logger from '../utils/logger.js';
-import sgMail from '@sendgrid/mail'
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-// const transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT,
-//     secure: true,
-//     auth: {
-//         user: process.env.SMTP_USER,
-//         pass:process.env.SMTP_PASS
-//     }
-// });
+const transporter = nodemailer.createTransport({
+    host: process.env.BREVO_SMTP_HOST,
+    port: process.env.BREVO_SMTP_PORT,
+    auth: {
+        user: process.env.BREVO_SMTP_USER,
+        pass: process.env.BREVO_SMTP_PASS
+    }
+});
 
-export const sendEmail = async (options)=>{
+const sendEmail = async ({ to, subject, html }) => {
     try {
         const mailOptions = {
-            // from: `SecureNation <${process.env.SMTP_USER}>`,
-            from:`SecureNation <${process.env.SENDGRID_FROM_EMAIL}>`,
-            to: options.to,
-            subject: options.subject,
-            text: options.text || undefined,
-            html:options.html || undefined
-        }
+            from: `SecureNation <${process.env.BREVO_FROM_EMAIL}>`,
+            to,
+            subject,
+            html
+        };
 
-        // await transporter.sendMail(mailOptions);
-        await sgMail.send(mailOptions)
-        logger.info("Mail sent successfully");
-        
+        await transporter.sendMail(mailOptions);
+        console.log(`Successfully email sent to ${to}`);
     } catch (error) {
-        logger.error("Failed to send email",error);
-        throw new Error("Email could not be sent")
+        console.error("Failed to send email");
+        console.error(error.message);
+        throw new Error("Email could not be sent");
     }
-}
+};
+
+export default sendEmail;
